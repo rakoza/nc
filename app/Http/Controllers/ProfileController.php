7b9763976c533d5db7ae6bc2d007b86e56242eb3
Predrag Rakonjac <predrag.rakonjac@gmail.com>
2022-04-15 14:41:15 +0200

    add profile controller and routes

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    /**
+     * Get profile editable date
+     *
+     * @return [type]             [description]
+     */
+    public function getProfile(Request $request)
+    {
+        $profile = Auth::user();
+
+        return $profile;
+    }
+
+    /**
+     * Update profile
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function updateProfile(Request $request)
+    {
+        $this->validate($request, [
+            'name'    => 'required|string|max:255',
+            'phone'   => 'nullable|string|max:255',
+            'address' => 'nullable|string|max:255',
+            'notes'   => 'nullable|string|max:1000',
+        ]);
+
+        $profile = Auth::user();
+
+        $profile->update([
+            'name'    => $request->name,
+            'phone'   => $request->phone,
+            'address' => $request->address,
+            'notes'   => $request->notes,
+        ]);
+
+        return $profile->only(['name','phone','address']);
+    }
+
+    /**
+     * Delete account
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function deleteAccount(Request $request)
+    {
+        $user = Auth::user();
+
+        // if($user->hasRole(['administrator'])) {
+        //     // 401 ce inicirati redirekciju (vidi api.js), zato sam stavio 404
+        //     abort(404, 'An administrator account is not allowed to be deleted.');
+        //     return;
+        // }
+
+        // brisemo user account
+        $user->delete();
+
+        return 'account deleted';
+    }
+}
--- routes/spa.php
+++ routes/spa.php
@@ -1,2 +1,12 @@
 <?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+
+// treba biti dozvoljeno svi korisnicima
+Route::get('profile', [ProfileController::class, 'getProfile'])->name('profile.get');
+Route::put('profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
+Route::delete('profile', [ProfileController::class, 'deleteAccount'])->name('account.delete');
+
 // place for protected routes, API routes for SPA
+Route::apiResource('users', UserController::class);
