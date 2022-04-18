9fcfddc76da5077e16113be96082f52d433bf7df
Predrag Rakonjac <predrag.rakonjac@gmail.com>
2022-04-18 11:02:37 +0200

    return user profile data from HomeController::check

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,7 +24,7 @@ class HomeController extends Controller
      */
     public function check()
     {
-        return 'cool';
+        return Auth::user();
     }
 
     /**
